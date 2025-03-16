6a4a7df86a1bdd31b7184ca8535964cac378b5d1
VrishabhPatni <vrishabh1702@gmail.com>
2025-03-16 19:25:09 +0530

    Footer of homepage added and homepage card fixed

--- frontend/components/Home_page_card.jsx
+++ frontend/components/Home_page_card.jsx
@@ -1,27 +1,29 @@
+import arrow from "../src/assets/Arrow.svg"
+
 function Card({ title, titleColor = 'blue', boxBackground = 'white', learnMoreColor = 'gray', image }) {
-  return (
-    <div
-      className={`border border-black w-[900px] h-[300px] p-10 pt-2 flex justify-between items-center shadow-md mx-auto rounded-2xl`}
-      style={{ backgroundColor: boxBackground }}
-    >
-      <div className="space-y-4">
-        <div
-          className={`text-4xl font-bold px-3 py-1 rounded-lg inline-block`}
-          style={{ backgroundColor: titleColor }}
-        >
-          {title}
+    return (
+      <div
+        className={`border border-black flex justify-between items-center shadow-xl mx-auto`}
+        style={{ backgroundColor: boxBackground ,borderRadius:"1.5rem", width:"650px", height:"300px" }}
+      >
+        <div className="mt-0">
+          <div
+            className={`text-3xl font-bold px-3 py-1 rounded-lg inline-block`}
+            style={{ backgroundColor: titleColor }}
+          >
+            {title}
+          </div>
+          <br />
+          <div className="flex items-center text-bold space-x-4 text-lg font-semibold">
+            <img src={arrow} alt="Arrow" />
+            <span> </span>
+            <span style={{ color: learnMoreColor }}>Learn more</span>
+          </div>
         </div>
-        <div className="flex items-center space-x-2 text-lg font-semibold">
-          <span className="text-black text-2xl">↗</span>
-          <span style={{ color: learnMoreColor }}>Learn more</span>
-        </div>
+        {image && <img src={image} alt="Diagram" className="w-[290px] h-[270px]" />}
       </div>
-      
-      {image && <img src={image} alt="Diagram" className="w-80 h-60 rounded-2xl" />}
-    
-    </div>
-
-  );
-}
-
-export default Card;
+    );
+  }
+  
+  export default Card;
+  
