c62b07712afa4f2df61e231bd7b5a5d6d4c7465f
VyankateshKulkarni06 <kulkarnivyankatesh06@gmail.com>
2025-03-18 00:58:50 +0530

    Changed import of app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './home'
-<<<<<<< HEAD
-import LoginBoard from './Login'
+// import LoginBoard from './Login'
 import Dashboard from "./dashboard"
 import Dashcard from "../components/DashboardCard"
-
-=======
 import LoginBoard from './AuthCard'
->>>>>>> dd36a6c (Login and SignUp page UI)
+
 
 function App() {
   return (
     // <Home/>
     // <LoginBoard/>
-    <Dashboard/>
+    // <Dashboard/>
     // <Dashcard title={"hellop"}/>
+    <LoginBoard />
   )
 }
 
